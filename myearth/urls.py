dd5387903cecdbf3bf811cc93ed6fb576c2b14b7
Amit <amit2rockpower@gmail.com>
2018-05-17 01:50:59 +0530

    removing /swachh/ now directly oopens the app

--- myearth/urls.py
+++ myearth/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^swachh/',include('swachh.urls'))
+    url(r'^',include('swachh.urls'))
 ]
--- static/cssjs/js/script.js
+++ static/cssjs/js/script.js
@@ -5,6 +5,7 @@ function genQuote() {
   var selected_Quote = quotes[randNum]
   tweetQuote = "https://twitter.com/intent/tweet?text="+ "%23myEarth " + "Today i am "+ selected_Quote + " %23trendzingo www.trendzingo.com"
   $('.twitter-share-button').attr('href', tweetQuote);
+  $('.twitter-share-button').attr('href', tweetQuote);
   var fb_share_link = "https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2F" + randNum.toString() + "html"+ "&amp;src=sdkpreparse"
   $('.fb-share-button').attr('href', fb_share_link)}
 
@@ -16,13 +17,13 @@ var quotes = ["Blank",
   "goint to feed few animals because they are adorable.",
   "taking only the stairs instead of using lifts",
   "planting a tree because they deserve it.",
-  "checking the air filter of my car and giving it a clean.",
-  "using microfiber cloth to clean my home, instead of chemicals.",
+  "checking the air filter of my car and giving it a clean",
+  "using microfiber cloth to clean my home, instead of chemicals",
 " saving one litre of extra water !",
 " switching off electric bulb for one hour extra ! ",
 " going to talk to a old person for a while !",
 " using public transport !",
-" purchaing appropriate reusable container instead of plastics.",
+" purchaing appropriate reusable container instead of plastics",
 "using reusable grocery bags rather than plastic bags",
 "Joining the carpool or public transportation to save fuel because they are too little.",
 "Replaceing regular light bulbs in your home with compact fluorescent bulbs",
